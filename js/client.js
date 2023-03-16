dd958d9323b2b7665ee4f391240bee68ec81ccab
ec <goncalves.erwan.pro@gmail.com>
2023-03-16 13:24:34 +0100

    sio2

--- js/client.js
+++ js/client.js
@@ -1,24 +1,67 @@
 var socket = io();
 
-socket.emit('set-pseudo', prompt("Pseudo ?"));
+var pseudo = prompt("Pseudo ?")
+var chat_select = 'general';
+socket.emit('set-pseudo', pseudo);
 
 var messages = document.getElementById('messages');
 var form = document.getElementById("form");
 var input = document.getElementById('message');
-var button = document.getElementById("disco")
+var id_salon = "salon";
+var lesMessages = [];
 
 
-form.addEventListener('submit', (e) =>{
-    e.preventDefault();
-    socket.emit('emission_message', input.value);
-    //input.value="";
-  let valueMes= input.value
-   console.log(valueMes);  
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let Message = {
+    emeteur : pseudo, 
+    recepteur : chat_select,
+    content : input.value
+  } 
+  socket.emit('emission_message', Message);
+  //input.value="";
+  
 })
 
-socket.on('reception_message', (contenu)=>{
-var message = document.createElement('li');
-message.innerHTML = contenu;
-messages.appendChild(message);
+socket.on('reception_message', (Message) => {
+  var message = document.createElement('li');
+  message.innerHTML = Message.content;
+  messages.appendChild(message);
+  lesMessages = [];
+  lesMessages.push(Message)
+  console.log("Message stockés" +lesMessages)
+
+  lesMessages.forEach(element => {
+    
+  });
 
 });
+
+function salon(id) {
+  chat_select = id
+  messages.innerHTML = ''
+
+}
+
+socket.on('reception_user', (users) => {
+  console.log(users)
+  var users_list = document.getElementById('users_list');
+  users_list.innerHTML = "<li><button id='general' onclick='salon('general')'>Géneral</button></li";
+  let user_item = users.map((user) => {
+    if (user.pseudo_client !== pseudo){
+      var list_item = document.createElement('li');
+      list_item.classList.add('user')
+      list_item.innerHTML = `<button id="button_user" onclick="salon('${user.pseudo_client}')">${user.pseudo_client}</button>`
+      return list_item
+    }else{
+      return null
+    }
+    
+  })
+  user_item.forEach(element => {
+    if (element !== null ){
+      users_list.appendChild(element)
+    }
+    
+  });
+})
--- server.js
+++ server.js
@@ -7,37 +7,50 @@ const io = new Server(server);
 var path = require("path");
 let PORT = 8082;
 
-server.listen(PORT, () =>{
-    console.log('serveur ' +PORT);
+server.listen(PORT, () => {
+    console.log('serveur ' + PORT);
 });
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/client', (req, res) =>{
+app.get('/client', (req, res) => {
     res.sendFile(path.join(__dirname, 'js/client.js'));
 });
 
-app.get('/style', (req, res) =>{
+app.get('/style', (req, res) => {
     res.sendFile(path.join(__dirname, 'css/style.css'));
 });
 
-io.on('connection', (socket)=>{
+var utilisateur = {};
 
-    socket.on('set-pseudo',(pseudo)=>{
-        console.log(pseudo + "vient de se connecter a "+new Date());
+io.on('connection', (socket) => {
+
+    socket.on('set-pseudo', (pseudo) => {
+        console.log(pseudo + "vient de se connecter a " + new Date());
         socket.nickname = pseudo;
-        console.log (pseudo);
+        console.log(pseudo);
+        utilisateur[socket.id] = {
+            id_client: socket.id,
+            pseudo_client: pseudo
+        };
+        io.emit('reception_user', Object.values(utilisateur));
+    })
+    socket.on('emission_message', (Message) => {
+        socket.broadcast.emit('reception_message', Message);
+        socket.emit('reception_message', Message);
+        console.log(Message);
+      
     });
-    socket.on('emission_message', (message)=>{
-        socket.broadcast.emit('reception_message', `${socket.nickname} + ${message}`);
-        socket.emit('reception_message', `${socket.nickname} + ${message}`);
-        console.log(message);
-        });
 
-       
-            
+    socket.on('disconnect', () =>{
+        console.log('utilisateur deconnecté' +socket.id);
+        delete utilisateur[socket.id];
+        io.emit('reception_user', Object.values(utilisateur));
+    })
+
+    
 })
 
 
